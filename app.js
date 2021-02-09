c212f0d998e8494df260e346c6d2b9baffd34893
Laith Alwani <laithalwani@gmail.com>
2021-02-08 23:40:43 -0500

    added a welcome function

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ const connection = mysql.createConnection({
 connection.connect(function(err){
     if(err) throw err;
     console.log(`connected as id ${connection.threadId}`);
+    welcome();
 });
+
+function welcome(){
+    inquirer.prompt([
+        {
+            type:'list',
+            message:'what would you like to do?',
+            name:'addViewUpdate',
+            choices:['Add','View','Update','Exit']
+        }
+    ]).then(response =>{
+        if(response.addViewUpdate === 'Add'){
+            addOptions()
+        }else if(response.addViewUpdate === 'View'){
+            viewDepartments();
+        }else if(response.addViewUpdate === 'Update'){
+            updateOptions();
+        }else{
+            connection.end();
+        }
+    });
+}
 
 
