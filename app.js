92a265e2256a7faf7f06eaccacee37434e0dfa3f
Laith Alwani <laithalwani@gmail.com>
2021-02-08 23:43:00 -0500

    added add options and created add department, add role methods and an insertInTable method

--- app.js
+++ app.js
@@ -37,4 +37,86 @@ function welcome(){
     });
 }
 
+function addOptions(){
+    inquirer.prompt([
+        {
+            type:'list',
+            message:'What do you like to add?',
+            name:'addAction',
+            choices:['Department','Role','Employee','Cancel']
+        }
+    ]).then(response=>{
+        if(response.addAction ===  'Department'){
+            addDepartment();
+        }else if(response.addAction ===  'Role'){
+            addRole();
+        }else if(response.addAction ===  'Employee'){
+            addEmployee();
+        }else{
+            welcome();
+        };
+    })
+}
 
+function addDepartment(){
+    inquirer.prompt(
+    [
+        {
+            type:'input',
+            message:`What is the new Department name`,
+            name: 'department'
+        }
+    ]).then(response =>{
+        const newDepartment = {
+            name:response.department
+        }
+        insertInTable('department', newDepartment);
+    });     
+}
+function addRole(){
+    connection.query('SELECT * FROM department', (err,res)=>{
+        if (err) throw err;
+        inquirer.prompt(
+            [
+                {
+                    type:'input',
+                    message:`What is the new title?`,
+                    name: 'title'
+                },
+                {
+                    type:'number',
+                    message:`What is the salary?`,
+                    name: 'salary'
+                },
+                {
+                    type:'list',
+                    message:'please enter a department id',
+                    name:'department',
+                    choices:  ()=>{
+                        const departments = res.map(department => department.name);
+                        return departments;
+                    }
+                }
+            ]).then(response =>{
+                const chosenDepartment = res.find(depratment => depratment.name === response.department);
+                
+                
+                const newRole = {
+                    title:response.title,
+                    salary: response.salary,
+                    department_id: chosenDepartment.id
+                 }
+                 insertInTable('role', newRole);
+            }); 
+    })
+    
+}
+
+function insertInTable(table,obj){
+    connection.query(`INSERT INTO ${table} SET ?`, obj,
+        (err,res)=>{
+            if(err) throw err;
+            console.log('Succesfully added');
+            welcome();
+        })
+}
