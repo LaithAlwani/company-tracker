58a370d8d8a6c9c0a822de4dcb6b1394f896e65b
Laith Alwani <laithalwani@gmail.com>
2021-02-08 23:39:06 -0500

    added seed.sql and connected to the data base

---
+++ seed.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manger_id INT
+);
